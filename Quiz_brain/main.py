2585e2370f6f5ba5d98d103379b9ae08e48e5dff
luch91 <61505908+luch91@users.noreply.github.com>
2023-04-29 02:17:01 +0100

    added readme

--- Quiz_brain/main.py
+++ Quiz_brain/main.py
@@ -2,7 +2,7 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-question_bank = [] # a list of question objects
+question_bank = []  # a list of question objects
 for question in question_data:
     question_text = question["text"]
     question_answer = question["answer"]
@@ -11,3 +11,8 @@ for question in question_data:
 
 quiz = QuizBrain(question_bank)
 
+while quiz.still_have_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
---
+++ Quiz_brain/readme.md
@@ -0,0 +1,2 @@
+A simple True/False game. A question will be asked to the user and they will choose True of False. 
+It will tell them if they are right or wrong and give them a score.
