1d10dbd4a8232298942d34b7780db0763e9e1959
luch91 <61505908+luch91@users.noreply.github.com>
2023-06-05 15:13:54 +0100

    added file to email

--- Sending Emails/main.py
+++ Sending Emails/main.py
@@ -1,3 +1,4 @@
+"""
 import smtplib
 
 my_email = "orluchee91@gmail.com"
@@ -10,5 +11,44 @@ with smtplib.SMTP("smtp.gmail.com") as connection:
                         to_addrs="luchijudith@gmail.com",
                         msg="Subject: Welcome Onboard\n\nThis is your appointment letter"
                         )
+
+
+"""
+
+import datetime as dt
+import random
+import smtplib
+
+"""
+now = dt.datetime.now()
+print(now)
+year = now.year
+month = now.month
+week_day = now.weekday()
+
+date_of_birth = dt.datetime(year=1990, month=2, hour=13)
+print(date_of_birth)
+
+"""
+my_email = "orluchee91@gmail.com"
+password = "orluchee-9191"
+
+now = dt.datetime.now()
+week_day = now.weekday()
+if week_day == 0:
+    with open("quotes.txt") as quote_files:
+        all_quotes = quote_files.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email, msg=f"Subject: Monday Motivation\n\n{quote}"
+        )
+
+
 
 
