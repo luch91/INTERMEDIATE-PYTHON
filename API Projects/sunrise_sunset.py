3d0f17d0edb5d0086c2ef7566fd09666de4f746c
luch91 <61505908+luch91@users.noreply.github.com>
2023-06-07 14:03:02 +0100

    added Quiz app file

--- API Projects/sunrise_sunset.py
+++ API Projects/sunrise_sunset.py
@@ -7,20 +7,24 @@ from datetime import datetime
 
 # longitude = data["iss_position"]["longitude"]
 # latitude = data["iss-position"]["latitude"]
-
 # iss_position = (longitude, latitude)
 # print(iss_position)
 LAT = 31.2638905
 LONG = -98.5456116
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+iss_longitude = float(data["iss_position"]["longitude"])
+iss_latitude = float(data["iss-position"]["latitude"])
 
 parameters = {
     "lat": LAT,
     "lng": LONG,
     "formatted": 0
 }
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
+
 sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
 sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 print(sunrise)
