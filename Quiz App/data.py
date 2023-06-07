9360f9319b3d516b438acc4c0bdb416492a28db0
luch91 <61505908+luch91@users.noreply.github.com>
2023-06-07 18:25:57 +0100

    added file

--- Quiz App/data.py
+++ Quiz App/data.py
@@ -1,3 +1,16 @@
+import requests
+parameters = {
+    "amount": 20,
+    "type" : "boolean"
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+data = response.json()
+question_data = (data["results"])
+
+
+"""
 question_data = [
     {
         "category": "Science: Computers",
@@ -100,3 +113,4 @@ question_data = [
         ]
     }
 ]
+"""
