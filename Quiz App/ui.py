344f515a06cf146b51687067d109586d1d2a7b4e
luch91 <61505908+luch91@users.noreply.github.com>
2023-06-18 23:06:35 +0100

    Completed QuizMe App

--- Quiz App/ui.py
+++ Quiz App/ui.py
@@ -1,10 +1,11 @@
 from tkinter import *
-
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("QuizMe")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -15,19 +16,32 @@ class QuizInterface:
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150, 125, text="Some Question Text",
                                                      fill=THEME_COLOR,
+                                                     width=200,
                                                      font=("Arial", 20, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_click)
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_click)
         self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
 
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_click(self):
+        self.quiz.check_answer("True")
+
+    def false_click(self):
+        self.quiz.check_answer("False")
 
 
 
